155d7978f5652066f51d105bcd568ee51875b511
jms181 <jacob.m.sansone@gmail.com>
2020-04-01 22:08:24 -0500

    Setup server socket for listening for connections

--- server.c
+++ server.c
@@ -2,157 +2,58 @@
 * Jacob Sansone
 * Alexander King
 * COP4635
-* Project 1
+* Project 2
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
+#include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAX_CONNECTIONS 5
 
-/*Used for error detection during all steps of creating and connecting the sockets.*/
-void error(char *msg)
-{
-    perror(msg);
-    exit(-1);
-}
-
-/*This is responsible for parsing the get request when recieved
- and pulling the name of the file out.*/
-char* parser(char *tokenize)
-{ 
-    char* startOfFile = strchr(tokenize, '/');
-    char *endOfFile = strchr(startOfFile, ' ');
-    char *buffer;
-
-    buffer = malloc(sizeof(char) *(endOfFile - startOfFile));
-
-    strncpy(buffer, startOfFile, endOfFile-startOfFile);
-
-    buffer[strlen(buffer)] = 0;
-
-    return buffer;
-}
+//Global variables
+int server_sockfd = 0, client_sockfd = 0, bdFail = 0, lisFail = 0;
 
 int main() 
 {
-    /*Creation and initialization of variables needed*/
-    char buffer[256], buff2[256];
-    char *filename;
-    struct sockaddr_in tcp_server_address;
-    int tcp_server_socket, tcp_client_socket , n, l, byte_size;
-    FILE *fp;
-
-    
-    
-    /*Creating the server socket*/
-    tcp_server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(tcp_server_socket < 0)
-    {
-        error("Error: Socket Creation Failure\n");
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_sockfd == -1){
+        printf("Failed to create socket");
+        exit(-1);
     }
-    printf("Socket Created Successfully\n");
 
-    
-    
-    /*Defining the paramters of the server socket: 
-       -------------------> The port to listen on
-       -------------------> The TCP/IP protocol it will follow
-       -------------------> Setting the IP to localhost*/
-    tcp_server_address.sin_family = AF_INET;
-    tcp_server_address.sin_port = htons(60056);
-    tcp_server_address.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in server_info, client_info;
+    int s_addrlen = sizeof(server_info);
+    int c_addrlen = sizeof(client_info);
+    memset(&server_info, 0, s_addrlen);
+    memset(&client_info, 0, c_addrlen);
+    server_info.sin_family = PF_INET;
+    server_info.sin_addr.s_addr = INADDR_ANY;
+    server_info.sin_port = htons(60056);
 
-
-
-
-
-    /*Binding the socket to the parameters we just set.*/
-    n = bind(tcp_server_socket, (struct sockaddr *) &tcp_server_address, sizeof(tcp_server_address));
-    if(n < 0)
-    {
-        error("Error: Socket Failed To Bind\n");
+    bdFail = bind(server_sockfd, (struct sockaddr *)&server_info, s_addrlen);
+    if(bdFail < 0){
+        printf("Failed to bind socket");
+        exit(-1);
     }
-    printf("Socket Successfully Bound To Port\n");
+    
+    lisFail = listen(server_sockfd, MAX_CONNECTIONS);
+    if(lisFail < 0){
+        printf("Failed to listen for connections");
+        exit(-1);
+    }
+
+    getsockname(server_sockfd, (struct sockaddr*) &server_info, (struct socklen_t*) &s_addrlen);
+    printf("Server started on: %s:%d\n", inet_ntoa(server_info.sin_addr), ntohs(server_info.sin_port));
+
     
 
 
-
-
-    /*Telling the socket to listen on the port for requests and setting an allowed number to be in the queue*/
-    l = listen(tcp_server_socket, 5);
-    if(l < 0)
-    {
-        error("Error: Socket Failed To Listen\n");
-    }
-    printf("Socket Is Listening For Connections\n");
     
-
-
-
-
-
-
-    /* A while loop that was made to continuously
-     wait for connection from either the client or the browser and handle the requests as given.*/
-    while(1)
-    {
-        tcp_client_socket = accept(tcp_server_socket, NULL, NULL);
-        printf("Client has connected\n");
-        byte_size = recv(tcp_client_socket, buffer, sizeof(buffer)-1, 0);
-        buffer[byte_size] = '\0';
-        filename = parser(buffer);
-        memset(buffer, 0, strlen(buffer));
-        /*If the file after parsing is the file we need.
-         Open and read through it sending the desired document back to the client if found,
-          otherwise error.*/
-        if(strcmp(filename, "/index.html") == 0)
-        {
-            fp = fopen("./index.html", "r");
-            if(fp == NULL){
-                error("Failed to read file");
-            }
-            else{
-                strncpy(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n", 256);
-                while (fgets(buff2, 256, fp) != NULL)
-                {
-                    strncat(buffer, buff2, 256);
-                }
-                fclose(fp);
-                buffer[sizeof(buffer)] = '\0';
-                write(tcp_client_socket, buffer, 256);
-                memset(buffer, 0, 256);
-            }
-        }
-        else if(strcmp(filename, "/index2.html") == 0)
-        {
-            fp = fopen("./index2.html", "r");
-            if(fp == NULL){
-                error("Failed to read file");
-            }
-            else{
-                strncpy(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n", 256);
-                while (fgets(buff2, 256, fp) != NULL)
-                {
-                    strncat(buffer, buff2, 256);
-                }
-                fclose(fp);
-                buffer[sizeof(buffer)] = '\0';
-                write(tcp_client_socket, buffer, 256);
-                memset(buffer, 0, 256);
-            }
-        }
-        else{
-            write(tcp_client_socket, "HTTP/1.1 404 NOT FOUND\r\n\r\n", 256);
-        }
-        /*And close the socket, free the fileName and clear the buffer when complete.*/
-        close(tcp_client_socket);
-        printf("Client has disconnected\n");
-        memset(filename, 0, strlen(filename));
-        free(filename);
-    }
     return 0;
 }
