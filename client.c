17bfaee9de7f9eeca0c60170c6d274dbb44ff349
jms181 <jacob.m.sansone@gmail.com>
2020-04-02 22:01:53 -0500

    Updating Errors

--- client.c
+++ client.c
@@ -58,7 +58,7 @@ int main()
    memset(&client_info, 0, c_addrlen);
    /*port num = 60056*/
    server_info.sin_family = PF_INET;
-   server_info.sin_addr.s_addr = inet_addr(INADDR_ANY);
+   server_info.sin_addr.s_addr = INADDR_ANY;
    server_info.sin_port = htons(60056);
    
    int err = connect(sockfd,(struct sockaddr *)&server_info, s_addrlen);
@@ -69,14 +69,14 @@ int main()
    }
    
    getsockname(sockfd,(struct sockaddr*) &client_info, (socklen_t *) &c_addrlen);
-   getpeername(sockfd,(struct sockaddr*) &server_info, (socklen_t *) &s_addrlen);
+   //getpeername(sockfd,(struct sockaddr*) &server_info, (socklen_t *) &s_addrlen);
    printf("Connect to Server: %s:%d\n", inet_ntoa(server_info.sin_addr), ntohs(client_info.sin_port));
    
-   /*send(sockfd, "hello", LENGTH_NAME, 0);*/
+   /*send(sockfd, "hello", LENGTH_NAME, 0);
    
-   pthread_t recv_msg_thread;
-
-	if((pthread_create(&recv_msg_thread,NULL,(void *) recv_msg_handler, NULL)!= 0))
+	pthread_t recv_msg_thread;
+	recv_msg_thread = pthread_create(&recv_msg_thread,NULL,(void *) recv_msg_handler, NULL);
+	if(recv_msg_thread != 0)
 	{
 		printf("Create pthread error!\n");
 		exit(EXIT_FAILURE);
@@ -90,7 +90,7 @@ int main()
 			break;
 		}
 	}
-	
+	*/
 	close(sockfd);
     return 0;
 }
