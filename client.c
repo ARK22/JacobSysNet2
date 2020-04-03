e3bd5552d81c75d5d07a2786bcb6acd221946c99
ark22 <ark22@students.uwf.edu>
2020-04-02 22:15:04 -0400

    fixed some

--- client.c
+++ client.c
@@ -44,7 +44,7 @@ void send()
 
 int main()
 {
-	sockfd = socket(AF_INET, SOCKS_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd == -1)
 	{
 		printf("Fail to create a socket");
@@ -58,7 +58,7 @@ int main()
    memset(&client_info, 0, c_addrlen);
    /*port num = 60056*/
    server_info.sin_family = PF_INET;
-   server_infor.sin_addr.s_addr = inet_addr(INADDR_ANY);
+   server_info.sin_addr.s_addr = inet_addr(INADDR_ANY);
    server_info.sin_port = htons(60056);
    
    int err = connect(sockfd,(struct sockaddr *)&server_info, s_addrlen);
@@ -72,7 +72,7 @@ int main()
    getpeername(sockfd,(struct sockaddr*) &server_info, (socklen_t *) &s_addrlen);
    printf("Connect to Server: %s:%d\n", inet_ntoa(server_info.sin_addr), ntohs(client_info.sin_port));
    
-   send(sockfd, "hello", LENGTH_NAME, 0);
+   /*send(sockfd, "hello", LENGTH_NAME, 0);*/
    
    pthread_t recv_msg_thread;
 
